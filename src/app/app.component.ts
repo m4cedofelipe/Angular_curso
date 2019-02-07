f8f52c7475b453fc590fcd5c1a02e702a0026e96
felipe macedo <felipe.m4cedo@gmail.com>
2019-02-07 00:13:56 -0200

    Ciclo de vida do Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+
+  valor: any = 5;
+
+  deletarCiclo: Boolean = false;
+
+  mudarValor() {
+    this.valor++;
+  }
+
+  destroiCiclo() {
+    this.deletarCiclo = true;
+  }
 }
